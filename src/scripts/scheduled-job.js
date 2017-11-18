64ce12580676b2101ee1ee0ff1c4a2b957e9cba4
tantan587 <yoni.h.silverman@gmail.com>
2017-11-18 09:01:30 -0600

    trying to connect to databsae with scheduled-job

--- src/scripts/scheduled-job.js
+++ src/scripts/scheduled-job.js
@@ -1,4 +1,25 @@
+require('dotenv').config()
+
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  searchPath: 'knex,users'
+});
+
+console.log(process.env.DATABASE_URL)
+
+//going to used for transactions
+//var Promise = require('bluebird')
+
+
 function sayHello() {
+    knex('passwords')
+    .select()
+    .then(result =>
+    {
+        console.log(result)
+    })
     console.log('Hello');
 }
 sayHello();
