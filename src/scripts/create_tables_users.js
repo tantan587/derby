de88899ec86b197c7d81d6b1b4092391af52d54f
camila8 <camilabenaim2012@u.northwestern.edu>
2017-11-19 11:18:28 -0600

    changing names
    leagues switch to users for script

---
+++ src/scripts/create_tables_users.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+
+var connectionstr = process.env.DATABASE_URL === undefined ? 'postgres://koperman:derbyapp123@localhost:5432/postgres' : process.env.DATABASE_URL
+var knex = require('knex')({
+  client: 'pg',
+  connection: connectionstr,
+  searchPath: 'knex,users'
+});
+
+
+function createInformation() {
+    knex.schema.withSchema('users').createTable('information', function (table) {
+    table.string('id')
+    table.string('username').primary();
+    table.string('first_name');
+    table.string('last_name');
+    table.string('email');
+    //table.timestamps();
+    })
+    .then(result =>
+        {
+            console.log(result)
+        })
+}
+
+function createPasswords() {
+    knex.schema.withSchema('users').createTable('passwords', function (table) {
+    table.string('username').primary();
+    table.string('passowrd')
+    })
+    .then(result =>
+        {
+            console.log(result)
+        })
+}
+
+createInformation();
+createPasswords();
+
+
+
+
