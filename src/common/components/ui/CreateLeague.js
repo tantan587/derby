d105f805b3d8b1eeccea3ab2132e3abe876aaa63
tantan587 <yoni.h.silverman@gmail.com>
2017-12-02 19:36:32 -0600

    updated create league

--- src/common/components/ui/CreateLeague.js
+++ src/common/components/ui/CreateLeague.js
@@ -7,6 +7,17 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import '../../../stylesheets/Menu.scss'
 import Divider from 'material-ui/Divider'
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import {
+  Table,
+  TableBody,
+  TableRow,
+	TableRowColumn,
+	TableHeaderColumn,
+	TableHeader,
+} from 'material-ui/Table';
 
 const style = {
 loginText: {
@@ -19,30 +30,18 @@ loginText: {
   loginTitle: {
     fontSize: '50px'
   },
-  loginBoard: {
-	position: 'absolute',
-  	top: '50%',
-  	left: '50%',
-  	margin: '-300px 0 0 -120px',
-	},
-
   sumbitButton: {
-	position: 'absolute',
+	position: 'relative',
   	top: '50%',
   	left: '50%',
-  	margin: '260px 0 0 -70px',
+  	margin: '0px 0 0 -70px',
 	},
-  block: {
-		maxWidth: 250,
-	  },
-	checkbox: {
-		marginTop: 30,
-		marginBottom: 15
-	  },
-	checkbox2: {
-		marginBottom: 16,
-		width: 'auto'
-	  },
+	tableText: {
+		textAlign: 'center',
+		fontSize: '16px',
+		whiteSpace: "normal",
+		wordWrap: "break-word"
+	}
 }
 
 class CreateLeague extends Component {
@@ -52,43 +51,28 @@ class CreateLeague extends Component {
 		this.pressSubmit = this.pressSubmit.bind(this)
 		this.keypress = this.keypress.bind(this)
 		this.handleSubmit = this.handleSubmit.bind(this)
-		this.onNumPlayersChange = this.onNumPlayersChange.bind(this)
 		this.updateEPLCheck = this.updateEPLCheck.bind(this)
         this.state={
-        	leauge_name:'',
-			leauge_password:'',
-			EPL:false,
-			numPlayers:0,
-			numPlayerError:false
+					leauge_name:'',
+					leauge_password:'',
+					EPL:false,
+					numPlayers:8,
+					privateInd:true,
+					team_name
         }
-	}
+	}	
 
-	onNumPlayersChange(e){
-		const n = e.target.value 
-		if ((n > 7 && n < 16) || n === "") {
-		   this.setState({numPlayers: e.target.value})
-		}
-		else {this.setState({numPlayers: 0})}
-	 }
-	
 	updateEPLCheck() {
 			this.setState({ EPL: !this.state.EPL });
 		  }
 
 	handleSubmit(e)
 	{
-		if(this.state.numPlayers > 7 && this.state.numPlayers < 16)
-		{
 			this.setState({ numPlayerError:false })
 			const { onLogin } = this.props
 			e.preventDefault()
 			alert("Good")
 			//onLogin(this.state.leauge_name, this.state.leauge_password)
-		}
-		else
-		{	
-			this.setState({ numPlayerError:true })
-		}
 	}
 
     pressSubmit(e) {
@@ -108,72 +92,106 @@ class CreateLeague extends Component {
 			    justifyContent:"center"
 			}
 		}
-		// if(this.props.user.loggedIn)
-		// {
-		// 	return (
-		// 		<Redirect to="/colormenu"/>
-		// 	)
-		// }
         return (
 		<div onKeyPress={(event) => this.keypress(event)}>
+			<MuiThemeProvider>
 			<div style={style.loginText}>
-			        <h1 style={style.loginTitle}>Create A League</h1>
-	        </div>
-	        <MuiThemeProvider>
-	          	<div>
-		          	<div style={style.loginBoard}>
-			       		<TextField
-			         		hintText="Enter League Name"
-			         		floatingLabelText="League Name"
-							 hintStyle={{textAlign: 'center' }}
-			         		/*style={styles.tabLink}*/
-			         		onChange = {(event,newValue) => this.setState({username:newValue})}/>
-			           	<br/>
-			         	<TextField
-			           		hintText="Enter League Password"
-			           		floatingLabelText="League Password"
-			           		onChange = {(event,newValue) => this.setState({password:newValue})}/>
-						<br/>
-						<TextField
-			           		hintText="(8-15)"
-			           		floatingLabelText="Number of Players"
-							errorText={this.state.numPlayerError ? "League Size can only be from 8 to 15" : ""}
-			           		onChange = {this.onNumPlayersChange}/>
-						<br/>
-						<Checkbox
-							label="Include English Premier League?"
-							checked={this.state.EPL}
-							onCheck={this.updateEPLCheck}
-							style={style.checkbox}
+				<h1 style={style.loginTitle}>Derby: Create A League</h1>
+			</div>
+			<div style={{paddingTop:150}}>
+				<Table>
+					<TableBody
+					displayRowCheckbox={false}
+					stripedRows={true}>
+					<TableRow style={{height:'150px'}}>
+							<TableRowColumn colSpan = "2" style={style.tableText}>Time to create your own league! Our default league
+								contains NBA, NFL, MLB, NHL, College Football, and NCAA Basketball.
+								You draft one team per conference in the professional sports,
+								and 3 teams from 3 different conferences in each college sport.
+								Have questions? Learn more about it here.
+								</TableRowColumn>
+						</TableRow>
+						<TableRow>
+							<TableRowColumn style={style.tableText}>League Name</TableRowColumn>
+							<TableRowColumn style={style.tableText}>
+							<TextField inputStyle={{textAlign: 'center'}}
+							onChange = {(event,newValue) => this.setState({leauge_name:newValue})}/>
+							</TableRowColumn>
+						</TableRow>
+						<TableRow>
+							<TableRowColumn style={style.tableText}>League Password</TableRowColumn>
+							<TableRowColumn style={style.tableText}>
+							<TextField inputStyle={{textAlign: 'center'}}
+								onChange = {(event,newValue) => this.setState({leauge_password:newValue})}/>
+							</TableRowColumn>
+						</TableRow>
+						<TableRow>
+							<TableRowColumn style={style.tableText}>Upload Image</TableRowColumn>
+							<TableRowColumn style={style.tableText}>
+							DOES NOT WORK YET
+							</TableRowColumn>
+						</TableRow>
+						<TableRow>
+							<TableRowColumn style={style.tableText}>Number of Teams</TableRowColumn>
+							<TableRowColumn style={style.tableText}>
+							<DropDownMenu value={this.state.numPlayers} onChange={(event, index, value) => this.setState({numPlayers:value})}>
+							<MenuItem value={8} primaryText="8 Teams" />
+							<MenuItem value={9} primaryText="9 Teams" />
+							<MenuItem value={10} primaryText="10 Teams" />
+							<MenuItem value={11} primaryText="11 Teams" />
+							<MenuItem value={12} primaryText="12 Teams" />
+							<MenuItem value={13} primaryText="13 Teams" />
+							<MenuItem value={14} primaryText="14Teams" />
+							<MenuItem value={15} primaryText="15 Teams" />
+						</DropDownMenu>
+							</TableRowColumn>
+						</TableRow>
+						<TableRow>
+							<TableRowColumn style={style.tableText}>Include the English Premier League?</TableRowColumn>
+							<TableRowColumn style={style.tableText}>
+							<Checkbox
+								label="Yes"
+								checked={this.state.EPL}
+								onCheck={this.updateEPLCheck}
+								style={{marginLeft:'37%'}}
 							/>
-						<TextField
-							floatingLabelText="(Optional) Emails for your Friends To Join this League"
-							hintText="Enter with Comma Seperated"
-							multiLine={true}
-							rows={2}
-							rowsMax={5}
-    					/><br />
-						<TextField
-							hintText="Player Name"
-							multiLine={true}
-							rows={2}
-							rowsMax={5}
-    					/>
-						<RaisedButton 
-			     			label="Submit"
-							style={style.sumbitButton} 
-			     			primary={true} 
-			             		/*style={style}*/
-		         		 	onClick={(event) => this.pressSubmit(event)}/>
-		         	</div>
-         		 	{/* <div>
-         		 		{
-         		 			(this.props.user.message !=="") ?
-         		 			<p>{this.props.user.message}</p> :
-         		 			<p></p>
-         		 		}
-         		 	</div> */}
-	         	</div>
+							</TableRowColumn>
+						</TableRow>
+						<TableRow>
+							<TableRowColumn style={style.tableText}>Public Or Private League?</TableRowColumn>
+							<TableRowColumn style={style.tableText}>
+							<RadioButtonGroup name="shipSpeed"
+							 defaultSelected="Private"
+							  style={{marginLeft:'37%'}}
+								onChange={(event, value) => this.setState({privateInd:value})}
+								value={this.state.numPlayers}>
+							<RadioButton
+								value={true}
+								label="Private"
+							/>
+							<RadioButton
+								value={false}
+								label="Public"
+							/>
+							</RadioButtonGroup>
+							</TableRowColumn>
+						</TableRow>
+						<TableRow>
+							<TableRowColumn style={style.tableText}>Your Team Name</TableRowColumn>
+							<TableRowColumn style={style.tableText}>
+							<TextField inputStyle={{textAlign: 'center'}}
+								onChange = {(event,newValue) => this.setState({leauge_password:newValue})}/>
+							</TableRowColumn>
+						</TableRow>									
+					</TableBody>
+				</Table>
+				<RaisedButton 
+								label="Submit"
+								style={style.sumbitButton}
+								labelStyle={{height: '20px', length:'50px'}}
+								primary={true} 
+								onClick={(event) => this.pressSubmit(event)}/>
+				</div>
 	     	</MuiThemeProvider>
 	  	</div>
 	  )
